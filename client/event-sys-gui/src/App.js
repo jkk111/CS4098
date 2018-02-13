dac20c1964ac30172d88f52ec800bdc9208cf3dd
tompywell <tpywell@gmail.com>
2018-02-13 11:13:08 +0000

    a simple login and sign up form

--- client/event-sys-gui/src/App.js
+++ client/event-sys-gui/src/App.js
@@ -35,6 +35,66 @@ class App extends Component {
           <br />
           Want to see the test results? <a href="?tests" >Click Here</a>
         </p>
+        <LoginForm/>
+        <SignupForm/>
+      </div>
+    );
+  }
+}
+
+class LoginForm extends React.Component {
+  render() {
+    return (
+      <div className="login-form">
+        <h1>Already Have An Account? / Log In</h1>
+        <form>
+          <label>
+            Username:<input type="text" name="loginUsername" />
+          </label>
+          <br/>
+          <label>
+            Password:<input type="text" name="loginPassword" />
+          </label>
+          <br/>
+          <input type="submit" value="Log In" />
+        </form>
+      </div>
+    );
+  }
+}
+
+class SignupForm extends React.Component {
+  render() {
+    return (
+      <div className="signup-form">
+        <h1>Sign Up / Create An Account</h1>
+        <form>
+          <label>
+            First Name:<input type="text" name="signupFirstName" />
+          </label>
+          <br/>
+          <label>
+            Last Name:<input type="text" name="signupLastName" />
+          </label>
+          <br/>
+          <label>
+            Username:<input type="text" name="signupUsername" />
+          </label>
+          <br/>
+          <label>
+            Email:<input type="text" name="signupEmail" />
+          </label>
+          <br/>
+          <label>
+            Password:<input type="text" name="signupPassword" />
+          </label>
+          <br/>
+          <label>
+            Confirm Password:<input type="text" name="signUpPasswordConfirm" />
+          </label>
+          <br/>
+          <input type="submit" value="Sign Up" />
+        </form>
       </div>
     );
   }
