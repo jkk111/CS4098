2faceeffed0b55370a9458ce5a84c032aab56f59
tompywell <tpywell@gmail.com>
2018-02-16 18:49:54 +0000

    ability for admins to view user list

--- client/event-sys-gui/src/ViewGuests.js
+++ client/event-sys-gui/src/ViewGuests.js
@@ -6,7 +6,14 @@ let ViewGuests = ({is_admin}) => {
   return <div className='view_guests'>
     <p>Here an admin can see list of all guests to all events ever</p>
     <p>to see guests for a specific event, go to that event and view the guestlist</p>
+    //<button onClick={this.fetchUserList}>Click Me!</button>
   </div>
 }
+
+// async fetchUserList(){
+//   let resp = await fetch('/admin/users')
+//   resp = await resp.json();
+//   console.log(resp);
+// }
 
 export default ViewGuests
--- client/event-sys-gui/src/ViewUsers.js
+++ client/event-sys-gui/src/ViewUsers.js
@@ -1,33 +1,59 @@
 import React from 'react';
 
-
 class ViewUsers extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       users: []
     }
-
-    this.refresh = this.refresh.bind(this)
+    this.handleChange = this.handleChange.bind(this);
     this.refresh();
+
+  }
+
+  handleChange(e) {
+    this.setState({users: e.currentTarget.value});
   }
 
   async refresh() {
     let resp = await fetch('/admin/users')
+    //console.log(resp);
     resp = await resp.json();
 
     this.setState({
       users: resp
     })
-
     console.log(resp)
   }
 
-  render() {
-    return <div className='view-users'>
-      Here an admin can see list of all guests to all events ever
 
-      to see guests for a specific event, go to that event and view the guestlist
+
+  render() {
+    //let data = this.props.users;
+    //console.log(this.state.users.length)
+    let data = this.state.users
+    console.log(data.length);
+    var rows = [];
+    for (var i=0; i<data.length; i++){
+      rows.push(
+        <tr>
+          <td>{data[i].id}</td>
+          <td>{data[i].f_name}</td>
+          <td>{data[i].l_name}</td>
+          <td>{data[i].email}</td>
+        </tr>
+      );
+    }
+    return <div>
+      <div className='view-users'>
+        <p>Here an admin can see list of all guests to all events ever</p>
+
+        <p>To see guests for a specific event, go to that event and view the guestlist</p>
+        <table>
+          <thead><tr><td>ID</td><td>First Name</td><td>Last Name</td><td>Email</td></tr></thead>
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
     </div>
   }
 }
