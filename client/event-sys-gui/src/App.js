0d856410499606d814a0c573e72758bbc3150cb9
jkk111 <kellyj58@tcd.ie>
2018-03-21 15:18:35 +0000

    Added login error message

--- client/event-sys-gui/src/App.js
+++ client/event-sys-gui/src/App.js
@@ -98,6 +98,13 @@ class App extends Component {
     })
     resp = await resp.json();
     this.props.set_logged_in(resp.auth_level);
+
+    if(resp.error === 'INVALID_AUTH') {
+      this.setState({
+        loginError: "Invalid Username/Password"
+      })
+    }
+
     set_cookie(resp.id)
   }
 
@@ -158,7 +165,7 @@ class App extends Component {
           <header className="App-header">
             <h1 className="App-title">Event-Management-System</h1>
           </header>
-          <LoginForm error={loginError} onSubmit={this.login} />
+          <LoginForm loginError={loginError} onSubmit={this.login} />
           <SignupForm registerError={registerError} onSubmit={this.register} />
         </div>
       );
