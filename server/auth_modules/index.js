29f5c8189352c755879e634b5a61aa9d4be3b805
jkk111 <kellyj58@tcd.ie>
2018-03-22 10:16:51 +0000

    Added updates + tickets + venue to event object

--- server/auth_modules/index.js
+++ server/auth_modules/index.js
@@ -10,31 +10,36 @@ app.use('/payments', payments.router)
 app.use('/raffle', raffle)
 
 app.get('/events', async(req, res) => {
-  let events = await Events.get('event', {}, [ 'id','name', 'description', 'venue_id', 'start_time', 'end_time', 'timezone', 'max_attendees' ]);
+  let events = await Events.get('event', {}, [ 'id','name', 'description', 'venue_id', 'start_time', 'end_time', 'max_attendees' ]);
   let venue_cache = {};
   let ticket_cache = {};
 
-  events.forEach(async(event) => {
+  for(var event of events) {
     let { id, venue_id } = event;
     let tickets = await Events.get('event_tickets', { event_id: id }, 'ticket_id');
-    tickets = tickets.map(async({ ticket_id }) => {
+    let updates = await Events.get('event_updates', { event_id: id }, '*');
+    for(var ticket of tickets) {
+      let { ticket_id } = ticket;
       if(ticket_cache[ticket_id]) {
-        return ticket_cache[ticket_id];
+        Object.assign(ticket, ticket_cache[ticket_id]);
       } else {
-        let ticket = await Events.get('tickets', { id: ticket_id }, '*')
-        ticket_cache[ticket_id] = ticket;
-        return ticket
+        let ticket_data = await Events.get('tickets', { id: ticket_id }, '*')
+        ticket_cache[ticket_id] = ticket_data[0];
+        Object.assign(ticket, ticket_cache[ticket_id])
       }
-    })
+    }
 
     if(venue_cache[venue_id]) {
       event.venue = venue_cache[venue_id];
     } else {
       let venue = await Events.get('venues', { id: venue_id }, '*')
-      venue_cache[venue_id] = venue;
-      event.venue = venue;
+      venue_cache[venue_id] = venue[0];
+      event.venue = venue_cache[venue_id];
     }
-  })
+
+    event.tickets = tickets;
+    event.updates = updates;
+  }
 
   res.json(events);
 });
