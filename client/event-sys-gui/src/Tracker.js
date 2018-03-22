f173cfbcd91c1a91e10bce00583bc6349286a77f
Evelyn Hayes <hayesev@tcd.ie>
2018-03-22 16:48:30 +0000

    Donation tracker broken

--- client/event-sys-gui/src/Tracker.js
+++ client/event-sys-gui/src/Tracker.js
@@ -5,14 +5,42 @@ class Tracker extends Component {
   constructor(props){
     super(props);
     this.state = {
+      expanded: false,
       count: this.props.count || 0
     }
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      expanded: !this.state.expanded
+    })
   }
 
   render(){
+
+    let { expanded } = this.state;
     let { count } = this.state;
-    return <div>
-      <h1> Current amount raised = {count} </h1>
+    let { name } = this.props;
+
+    let content = null;
+
+     if(expanded) {
+      
+      content = <div className='event-content'>
+        <span className='event-content-key'>Name</span>
+        <span className='event-content-value'>{name}</span>
+        <h1> Current amount raised = {count} </h1>
+      </div>
+    }
+
+    let event_name_class = expanded ? 'event-name-expanded' : 'event-name-collapsed'
+
+
+    return 
+    <div className='event' >
+      <div className={event_name_class} onClick={this.toggle}>{name}</div>
+      {content}
     </div>
   }
 }
