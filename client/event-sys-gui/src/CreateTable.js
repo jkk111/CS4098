59854586c01339ebe6631d46644ca8c4708e0472
lanead <lanead@tcd.ie>
2018-03-23 12:28:58 -0700

    Added basic tables

---
+++ client/event-sys-gui/src/CreateTable.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import * as R from 'ramda';
+import { Layer, Rect, Stage,} from 'react-konva';
+
+
+class CreateTable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: 'black',
+      height: 100,
+      width: 50,
+      children: [],
+    };
+  }
+
+
+  handleClick = (e) => {
+
+      
+      let drawingShape = <Rect
+        x={0}
+        y={0}
+        width={this.state.width}
+        height={this.state.height}
+        stroke={'black'}
+        fill={this.state.color}
+        key={this.state.children.length}
+        name={`shape${this.state.children.length}`}
+        draggable={true}
+      />
+
+      const { children } = this.state;
+      const newChildren = R.clone(children);
+      newChildren.push(drawingShape);
+
+      this.setState({
+        children: newChildren,
+      });
+    }
+
+
+  render() {
+   return (
+      <div>
+        <div className='form-button form-field' onClick={this.handleClick}>Add Table</div>
+        <Stage width={window.innerWidth} height={window.innerHeight}
+        >
+          <Layer ref='layer'>
+            {this.state.children}
+          </Layer>
+        </Stage>
+      </div>
+    );
+  }
+}
+
+export default CreateTable
