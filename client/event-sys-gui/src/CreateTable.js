b74882d92bde65189429f7e2f1c69cb3d59f63d1
jkk111 <kellyj58@tcd.ie>
2018-03-24 17:20:46 +0000

    Fixed code style

--- client/event-sys-gui/src/CreateTable.js
+++ client/event-sys-gui/src/CreateTable.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import * as R from 'ramda';
 import { Layer, Rect, Stage } from 'react-konva';
 
+
+const FILL_COLOR = 'brown'
+const STROKE_COLOR = 'black'
+const TABLE_WIDTH = 100;
+const TABLE_HEIGHT = 100;
+
+console.log(FILL_COLOR, TABLE_HEIGHT, TABLE_WIDTH)
+
+let Table = ({ x, y, updatePosition }) => {
+  console.log(updatePosition)
+  return <Rect x={x} y={y} fill={FILL_COLOR} draggable={true} onDragEnd={updatePosition}
+               width={TABLE_WIDTH} height={TABLE_HEIGHT} stroke={STROKE_COLOR} />
+}
 
 class CreateTable extends React.Component {
   constructor(props) {
@@ -9,101 +21,112 @@ class CreateTable extends React.Component {
 
     this.state = {
       addTable: false,
-      color: 'brown',
-      height: 100,
-      width: 100,
-      children: [],
       x: this.props.x,
-      y: this.props.y,
-      mouse: {
-        x: 0,
-        y: 0
-      },
+      y: this.props.y
     };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.updatePosition = this.updatePosition.bind(this);
+    this.mouseMove = this.mouseMove.bind(this);
+    this.startAddTable = this.startAddTable.bind(this);
+    this.endAddTable = this.endAddTable.bind(this);
   }
 
+  updatePosition(i) {
+    return (e) => {
+      let x = e.evt.dragEndNode.attrs.x;
+      let y = e.evt.dragEndNode.attrs.y;
+      let { tables } = this.state;
+      let before = tables.slice(0, i);
 
-    updatePosition = (e,x, y) => {
-    this.setState({
-      ...this.state,
-      x: e.evt.dragEndNode.attrs.x,
-      y: e.evt.dragEndNode.attrs.y,
-    });
-  }
-
-
-  handleClick = (e) => {
-
-      if (!this.state.addTable) return;
-
-      const { mouse } = this.state;
-      
-      let drawingShape = <Rect
-        x={mouse.x-50}
-        y={mouse.y-50}
-        width={this.state.width}
-        height={this.state.height}
-        stroke={'black'}
-        fill={this.state.color}
-        key={this.state.children.length}
-        name={`shape${this.state.children.length}`}
-        draggable={true}
-        onDragEnd={this.updatePosition}
-      />
-
-      const { children } = this.state;
-      const newChildren = R.clone(children);
-      newChildren.push(drawingShape);
+      let after = tables.slice(i + 1);
+      let table = { x: x, y: y }
 
       this.setState({
-        children: newChildren,
-      });
+        tables: [ ...before, table, ...after ]
+      })
     }
-
-  handleMouseMove= (e) => {
-      // get cursor current position
-    this.setState({
-      mouse: {
-        ...this.state.mouse,
-        x: e.evt.layerX,
-        y: e.evt.layerY,
-      }
-    });
   }
 
-  handleStartAddTableClick = (e) => {
+  mouseMove(e) {
+    this.setState({
+      x: e.evt.layerX,
+      y: e.evt.layerY
+    })
+  }
+
+  handleClick(e) {
+    let { addTable, tables = [], x, y } = this.state;
+    if(addTable) {
+      console.log(e, tables);
+      let next = { x, y };
+
+      this.setState({
+        tables: [ ...tables, next ]
+      })
+    }
+  }
+
+  // handleClick = (e) => {
+
+      // if (!this.state.addTable) return;
+
+      // const { mouse } = this.state;
+
+      // let drawingShape = <Rect
+      //   x={mouse.x-50}
+      //   y={mouse.y-50}
+      //   width={TABLE_WIDTH}
+      //   height={TABLE_HEIGHT  }
+      //   stroke={'black'}
+      //   fill={FILL_COLOR}
+      //   key={this.state.children.length}
+      //   name={`shape${this.state.children.length}`}
+      //   draggable={true}
+      //   onDragEnd={this.updatePosition}
+      // />
+
+      // const { children } = this.state;
+      // newChildren.push(drawingShape);
+
+      // this.setState({
+      //   children: newChildren,
+      // });
+    // }
+
+  startAddTable(e) {
     this.setState({
       addTable: true
     });
   }
 
-  handleStopAddTableClick = (e) => {
+  endAddTable(e) {
     this.setState({
       addTable: false
     });
   }
 
   render() {
-   return (
-      <div>
-        <div className='form-button form-field' onClick={this.handleStartAddTableClick}>Start Adding Tables</div>
-        <div className='form-button form-field' onClick={this.handleStopAddTableClick}>Stop Adding Tables</div>
-        <Stage width={window.innerWidth} height={window.innerHeight}  visible={true} onContentClick={this.handleClick} onContentMouseMove={this.handleMouseMove} 
-        >
-          <Layer ref='layer'>
-            <Rect
-              x={0}
-              y={0}
-              width={window.innerWidth}
-              height={window.innerHeight}
-              stroke={'black'}
-              fill ={'grey'}
-            />
-            {this.state.children}
-          </Layer>
-        </Stage>
-      </div>
-    );
+    let { tables = [] } = this.state;
+    let children = tables.map((table, i) => <Table key={i} {...table} updatePosition={this.updatePosition(i)} />)
+    return <div>
+      <div className='form-button form-field' onClick={this.startAddTable}>Start Adding Tables</div>
+      <div className='form-button form-field' onClick={this.endAddTable}>Stop Adding Tables</div>
+      <Stage width={window.innerWidth} height={window.innerHeight}
+             visible={true} onContentClick={this.handleClick}
+             onContentMouseMove={this.mouseMove}>
+        <Layer ref='layer'>
+          <Rect
+            x={0}
+            y={0}
+            width={window.innerWidth}
+            height={window.innerHeight}
+            stroke={'black'}
+            fill ={'grey'} />
+          {children}
+        </Layer>
+      </Stage>
+    </div>
   }
 }
 
