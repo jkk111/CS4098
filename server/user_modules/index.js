8e24763f3ba6a81a87c88f1caa828189d92c22fd
jkk111 <kellyj58@tcd.ie>
2018-03-26 12:57:07 +0100

    Tweaks to table editor, removed deprectated endpoint

--- server/user_modules/index.js
+++ server/user_modules/index.js
@@ -46,36 +46,6 @@ app.get('/info', async(req, res) => {
   }
 });
 
-app.get('/events', async(req, res) => {
-  let events = await Events.get('event', {}, [ 'id','name', 'venue_id', 'start_time', 'end_time', 'description', 'max_attendees' ]);
-  let venue_cache = {};
-  let ticket_cache = {};
-
-  events.forEach(async(event) => {
-    let { id, venue_id } = event;
-    let tickets = await Events.get('event_tickets', { event_id: id }, 'ticket_id');
-    tickets = tickets.map(async({ ticket_id }) => {
-      if(ticket_cache[ticket_id]) {
-        return ticket_cache[ticket_id];
-      } else {
-        let ticket = await Events.get('tickets', { id: ticket_id }, '*')
-        ticket_cache[ticket_id] = ticket;
-        return ticket
-      }
-    })
-
-    if(venue_cache[venue_id]) {
-      event.venue = venue_cache[venue_id];
-    } else {
-      let venue = await Events.get('venues', { id: venue_id }, '*')
-      venue_cache[venue_id] = venue;
-      event.venue = venue;
-    }
-  })
-
-  res.json(events);
-});
-
 app.post('/update_info', bodyParser.json(), async(req, res) => {
   let { f_name, l_name, email, phone, subscribed, allergens = [], accessibility } = req.body;
   let id = req.user_id;
