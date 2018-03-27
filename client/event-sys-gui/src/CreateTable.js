8810f5cad4b154c5e7545cc503a15e49a6248e6e
lanead <lanead@tcd.ie>
2018-03-27 09:20:13 -0700

    Konva Mobile Tables Size fix and Tap fix

--- client/event-sys-gui/src/CreateTable.js
+++ client/event-sys-gui/src/CreateTable.js
@@ -16,44 +16,74 @@ const ALIGN = 'center'
 const TEXT_WIDTH = 100;
 const TEXT_HEIGHT = 100;
 const TEXT_OFFSET = 30;
+const MOBILE_TABLE_WIDTH = 50;
+const MOBILE_TABLE_HEIGHT = 50;
+const MOBILE_TABLE_OFFSET = 25;
+const MOBILE_TEXT_WIDTH = 50;
+const MOBILE_TEXT_HEIGHT = 50;
+const MOBILE_TEXT_OFFSET = 15;
+const MOBILE_FONT_SIZE = 24;
 
 console.log(FILL_COLOR, TABLE_HEIGHT, TABLE_WIDTH)
 
-let Table = ({ x, y, updatePosition, updateFocused, focused }) => {
+let Table = ({ x, y, updatePosition, updateFocused, focused, mobileView}) => {
   let stroke_color = STROKE_COLOR;
+  let table_height = TABLE_HEIGHT;
+  let table_width = TABLE_WIDTH;
+  let table_offset = TABLE_OFFSET;
 
   if(focused) {
     stroke_color = FOCUSED_STROKE_COLOR;
   }
 
-  return <Rect x={x-(TABLE_OFFSET)}
-               y={y-(TABLE_OFFSET)}
+  else if(mobileView){
+    table_height = MOBILE_TABLE_HEIGHT;
+    table_width = MOBILE_TABLE_WIDTH;
+    table_offset = MOBILE_TABLE_OFFSET;
+  }
+
+  return <Rect x={x-(table_offset)}
+               y={y-(table_offset)}
                fill={FILL_COLOR}
                draggable={true}
                onDragStart={updateFocused}
                onDragEnd={updatePosition}
                onTouchStart={updateFocused}
                onTouchEnd={updatePosition}
-               width={TABLE_WIDTH}
-               height={TABLE_HEIGHT}
+               width={table_width}
+               height={table_height}
                stroke={stroke_color}
                perfectDrawEnabled={false} />
 }
 
-let TableText = ( {x, y, id, dragging, focused} ) => {
+let TableText = ( {x, y, id, dragging, focused, mobileView} ) => {
   let visBool= true;
+  let text_width = TEXT_WIDTH;
+  let text_height = TEXT_HEIGHT;
+  let table_offset = TABLE_OFFSET;
+  let text_offset = TEXT_OFFSET;
+  let font_size = FONT_SIZE;
 
   if(dragging && focused) {
     visBool = false;
   }
-  return <Text x={x-(TABLE_OFFSET)}
-               y={y-(TEXT_OFFSET)}
-               fontSize={FONT_SIZE}
+
+  else if(mobileView){
+    text_width = MOBILE_TEXT_WIDTH;
+    text_height = MOBILE_TEXT_HEIGHT;
+    table_offset = MOBILE_TABLE_OFFSET;
+    text_offset = MOBILE_TEXT_OFFSET;
+    font_size = MOBILE_FONT_SIZE;
+  }
+
+  return <Text x={x-(table_offset)}
+               y={y-(text_offset)}
+               fontSize={font_size}
                fontStyle={FONT_STYLE}
                align={ALIGN}
                stroke={STROKE_COLOR}
-               width={TEXT_WIDTH}
-               height={TEXT_HEIGHT}
+               width={text_width}
+               height={text_height}
                text={id}
                listening={false}
                visible={visBool}/>
@@ -133,31 +163,36 @@ class CreateTable extends React.Component {
     return (e) => {
       console.log(e.evt);
       let dragNode = e.evt.dragEndNode;
+      if(dragNode != 'undefined'){
+        let x = dragNode.attrs.x+(TABLE_OFFSET);
+        let y = dragNode.attrs.y+(TABLE_OFFSET);
+        let { tables} = this.state;
 
-      let x = dragNode.attrs.x+(TABLE_OFFSET);
-      let y = dragNode.attrs.y+(TABLE_OFFSET);
-      let { tables} = this.state;
+        let before = tables.slice(0, i);
 
-      let before = tables.slice(0, i);
+        let after = tables.slice(i + 1);
+        let table = { x: x, y: y }
 
-      let after = tables.slice(i + 1);
-      let table = { x: x, y: y }
-
-      this.setState({
-        focused: i,
-        dragging: false,
-        tables: [ ...before, table, ...after ]
-      })
+        this.setState({
+          focused: i,
+          dragging: false,
+          tables: [ ...before, table, ...after ]
+        })
+      }
     }
   }
 
   tapped(e){
+    if((typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1)){
+      this.setState({mobileView: true})
+    }
     this.setState({
       x: e.evt.changedTouches[0].clientX,
       y: e.evt.changedTouches[0].clientY
     })
     let {x,y} = this.state;
     console.log("X:" + x + " Y:" + y)
+    this.handleClick(e);
 
   }
 
@@ -196,12 +231,12 @@ class CreateTable extends React.Component {
   }
 
   render() {
-    let { tables = [], containerWidth, containerHeight, focused, dragging } = this.state;
-    let children = tables.map((table, i) => <Table key={i} {...table} updatePosition={this.updatePosition(i)} updateFocused={this.updateFocused(i)} focused={focused === i} />)
-    let children2 = tables.map((table, i) => <TableText key={i} {...table} id={i+1} dragging={dragging} focused={focused === i}/>)
+    let { tables = [], containerWidth, containerHeight, focused, dragging, mobileView } = this.state;
+    let children = tables.map((table, i) => <Table key={i} {...table} updatePosition={this.updatePosition(i)} updateFocused={this.updateFocused(i)} focused={focused === i} mobileView={mobileView} />)
+    let children2 = tables.map((table, i) => <TableText key={i} {...table} id={i+1} dragging={dragging} focused={focused === i} mobileView={mobileView} />)
     return <div>
       <Stage axisX={containerWidth/70} width={containerWidth-(containerWidth/60)} height={containerHeight-(containerHeight/8)}
-             visible={true} onContentClick={this.handleClick} onTap={this.handleClick} onTouchStart={this.tapped}
+             visible={true} onContentClick={this.handleClick} onTap={this.tapped}
              onContentMouseMove={this.mouseMove} >
         <Layer axisX={containerWidth/70} ref='layer' batchDraw={true}>
           <Rect
