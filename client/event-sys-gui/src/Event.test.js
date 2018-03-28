92d999a1fed40cb0f84136ad3ef0a011de4b6269
tompywell <tpywell@gmail.com>
2018-03-28 17:54:53 +0100

    i made a test

---
+++ client/event-sys-gui/src/Event.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Event from './Event.js';
+import ReactDOM from 'react-dom';
+
+describe('User', () => {
+  it('should be defined', () => {
+    expect(Event).toBeDefined();
+  });
+});
